23e5ce9b77ef4e5cd3da1dd1a78acbe7e65b41f0
jose.a <jose.a@numeto.com>
2018-11-12 14:07:59 +0100

    Created basic single page app with routing;

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact me</h2>
+                <p>Send me an email</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
